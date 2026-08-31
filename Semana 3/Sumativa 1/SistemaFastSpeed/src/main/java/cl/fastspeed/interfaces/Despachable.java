03dd065f6dff2e7248680a31e4af7f106d725ddf
Luis Ortiz A <meikodunois@live.com>
2026-08-31 16:11:25 -0400

    Delete Semana 3/Sumativa 1/SistemaFastSpeed/src/main/java/cl/fastspeed directory

--- Semana 3/Sumativa 1/SistemaFastSpeed/src/main/java/cl/fastspeed/interfaces/Despachable.java
+++
@@ -1,5 +0,0 @@
-package cl.fastspeed.interfaces;
-// Interfaz que define el contrato del comportamiento Despachable
-public interface Despachable {
-    void despachar();
-}
--- Semana 3/Sumativa 1/SistemaFastSpeed/src/main/java/cl/fastspeed/interfaces/Rastreable.java
+++
@@ -1,5 +0,0 @@
-package cl.fastspeed.interfaces;
-// Interfaz que define el contrato del comportamiento Rastreable
-public interface Rastreable {
-    void verHistorial();
-}
